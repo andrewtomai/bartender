b49212074b69649bb3e695f15ca38a5ae1f2435a
Andrew Tomai <tomaiandrew@gmail.com>
2020-12-27 10:16:02 -0800

    Implement craeting a drink with tags

--- packages/bartender-service/src/resolvers/Drink.ts
+++ packages/bartender-service/src/resolvers/Drink.ts
@@ -1,33 +1,64 @@
 import { v4 as uuid } from 'uuid';
+import * as R from 'ramda';
 import * as DynamoDb from '../helpers/dynamodb';
 import { Maybe, Drink, DrinkInput } from '../generated/graphql-types';
 
-const drinkId = (databaseDrinkId: string) => databaseDrinkId.split('DRINK#').join('');
-const databaseDrinkId = (id?: string) => `DRINK#${id || uuid()}`;
+const extractDrinkId = (databaseDrinkId: string) => databaseDrinkId.split('DRINK#').join('');
+const extractTagId = (databaseTagId: string) => databaseTagId.split('TAG#').join('');
+
+const enrichDatabaseDrinkId = (id: string) => `DRINK#${id}`;
+const enrichDatabaseTagId = (id: string) => `TAG#${id}`;
+
+// Enrich for retrieval from the database
+const enrichInputDrink = (drinkId: string, name: string): DynamoDb.DatabaseDrink => ({
+    ...enrichDrinkKeyPair(drinkId),
+    createdAt: new Date().toISOString(),
+    name,
+});
+
+const enrichInputTags = (drinkId: string) => (tagId: string): DynamoDb.DatabaseKeyPair => ({
+    primaryId: enrichDatabaseDrinkId(drinkId),
+    secondaryId: enrichDatabaseTagId(tagId),
+});
+
+const enrichDrinkKeyPair = (drinkId: string): DynamoDb.DatabaseKeyPair => ({
+    primaryId: enrichDatabaseDrinkId(drinkId),
+    secondaryId: enrichDatabaseDrinkId(drinkId),
+});
+
+// Format for output from database
+const formatTag = (databaseTag: DynamoDb.DatabaseItem) => ({ id: extractTagId(databaseTag.secondaryId) });
+
+const formatDrink = (databaseDrink: DynamoDb.DatabaseDrink, databaseTags?: DynamoDb.DatabaseItem[]): Drink => ({
+    id: extractDrinkId(databaseDrink.primaryId),
+    name: databaseDrink.name,
+    tags: databaseTags ? R.map(formatTag, databaseTags) : null,
+});
 
 export const drinkResolver = async (parent: unknown, args: { id: string }): Promise<Maybe<Drink>> => {
     const { id } = args;
-    const databaseId = databaseDrinkId(id);
+    const databaseId = enrichDatabaseDrinkId(id);
     const drink = await DynamoDb.getItem({ primaryId: databaseId, secondaryId: databaseId });
     if (!drink) return null;
     return {
-        id: drinkId(drink.primaryId),
+        id: extractDrinkId(drink.primaryId),
         name: drink.name as string,
     };
 };
 
+export const tagsForDrinkResolver = async (parent: Drink): Promise<Drink[]> => {
+    const { tags } = parent;
+    const ids = R.pluck('id', tags);
+    const tagKeyPairs: DynamoDb.DatabaseKeyPair[] = R.map(enrichDrinkKeyPair, ids);
+    const tagItems = await DynamoDb.getItems(tagKeyPairs);
+    return R.map(formatDrink, tagItems);
+};
+
 export const createDrinkResolver = async (parent: unknown, args: { drink: DrinkInput }): Promise<Drink> => {
-    const { drink } = args;
-    const id = databaseDrinkId();
-    const createdAt = new Date().toISOString();
-    await DynamoDb.putItem({
-        primaryId: id,
-        secondaryId: id,
-        createdAt,
-        ...drink,
-    });
-    return {
-        id: drinkId(id),
-        name: drink.name,
-    };
+    const { name, tags = [] } = args.drink;
+    const id = uuid();
+    const drinkItem = enrichInputDrink(id, name);
+    const tagItems = R.map(enrichInputTags(id), tags);
+    await DynamoDb.putItems([drinkItem, ...tagItems]);
+    return formatDrink(drinkItem, tagItems);
 };
--- packages/bartender-service/test/integration/drink.test.ts
+++ packages/bartender-service/test/integration/drink.test.ts
@@ -1,128 +1,32 @@
-import Query from './helpers/client';
+import { createDrink } from './helpers/client';
 import * as Validate from './helpers/Validate';
 
 describe('Scenario: Interacting with Drinks', () => {
-    let mixedDrinkId, whiskeyDrinkId;
     describe('Given I want to create a drink', () => {
         describe('When I create the drink using a name', () => {
             const name = 'mixed drink';
-            let response;
-            before('create the mixed drink', async () => {
-                const q = `#graphql
-                    mutation CreateDrink($drink: DrinkInput!) {
-                        createDrink(drink: $drink) {
-                            id,
-                            name,
-                        }
-                    }
-                `;
-                response = await Query(q, { drink: { name } });
-                mixedDrinkId = response.data.createDrink.id;
-            });
             it('Then I get back my mixed Drink', async () => {
-                Validate.drink({ name }, response);
-            });
-            it('And I can now retrieve the mixed drink using its Id', async () => {
-                const q = `#graphql
-                    query GetDrink($id: UUID!) {
-                        drink(id:  $id) {
-                            id,
-                            name
-                        }
-                    }
-                `;
-                const getDrinkResponse = await Query(q, { id: mixedDrinkId });
-                Validate.drink({ name }, getDrinkResponse);
+                const response = await createDrink({ name });
+                Validate.createdDrink({ name }, response);
             });
         });
-        describe.skip('When I create the drink using a name and a recipe', () => {
-            const name = 'whiskey';
-            const recipe = [{ name: 'whiskey', quantity: '2oz' }];
-            let response;
-            before('create the mixed drink', async () => {
-                const q = `#graphql
-                    mutation CreateDrink($drink: DrinkInput!) {
-                        createDrink(drink: $drink) {
-                            id,
-                            name,
-                            recipe {
-                                ingrediant {
-                                    name
-                                },
-                                quantity
-                            }
-                        }
-                    }
-                `;
-                response = await Query(q, { drink: { name: name, recipe } });
-                whiskeyDrinkId = response.data.id;
-            });
-            it('Then I get back my mixed Drink', async () => {
-                Validate.drink({ name, recipe }, response);
-            });
-            it('And I can now retrieve the mixed drink using its Id', async () => {
-                const q = `#graphql
-                    query GetDrink($drinkId: UUID!) {
-                        drink(id:  $id) {
-                            id,
-                            name,
-                            recipe {
-                                ingrediant {
-                                    name
-                                },
-                                quantity
-                            }
-                        }
-                    }
-                `;
-                const getDrinkResponse = await Query(q, { drinkId: whiskeyDrinkId });
-                Validate.drink({ name, recipe }, getDrinkResponse);
-            });
-        });
-        describe.skip('When I create the drink using a name, a recipe, and tags of other drinks', () => {
+        describe('When I create the drink using a name and tags of other drinks', () => {
             const name = 'whiskey sour';
-            const recipe = [
-                { name: 'whiskey', quantity: '2oz' },
-                { name: 'sour mix', quantity: '1oz' },
-            ];
-            const tags = [mixedDrinkId, whiskeyDrinkId];
-            let response;
-            before('create the mixed drink', async () => {
-                const q = `#graphql
-                    mutation CreateDrink($drink: DrinkInput!) {
-                        createDrink(drink: $drink) {
-                            id,
-                            name,
-                        }
-                    }
-                `;
-                response = await Query(q, { drink: { name: name, recipe, tags } });
-                whiskeyDrinkId = response.data.id;
+            let tagId;
+            before('create the whiskey drink to use as a tag', async () => {
+                const drinkForTag = await createDrink({ name: 'whiskey' });
+                tagId = drinkForTag.data?.createDrink.id;
             });
-            it('Then I get back my mixed Drink', async () => {
-                Validate.drink({ name, recipe, tags }, response);
-            });
-            it('And I can now retrieve the mixed drink using its Id', async () => {
-                const q = `#graphql
-                    query GetDrink($drinkId: UUID!) {
-                        drink(id:  $id) {
-                            id,
-                            name,
-                            recipe {
-                                ingrediant {
-                                    name
-                                },
-                                quantity
-                            },
-                            tags {
-                                id
-                            }
-                        }
-                    }
-                `;
-                const getDrinkResponse = await Query(q, { drinkId: response.data.id });
-                Validate.drink({ name, recipe, tags }, getDrinkResponse);
+            it('Then I get back my whiskey sour Drink', async () => {
+                const response = await createDrink({ name, tags: [tagId] });
+                const expectedTags = [{ id: tagId, name: 'whiskey' }];
+                Validate.createdDrink({ name, tags: expectedTags }, response);
             });
         });
+    });
+    describe.skip('When I create the drink using a name and a recipe', () => {
+        // const name = 'whiskey';
+        // const recipe = [{ name: 'whiskey', quantity: '2oz' }];
+        // let response;
     });
 });
