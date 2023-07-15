4cfadc2b5154f58e69d71db68f69b038d063456d
Andrew Tomai <andrew.tomai@acuitybrands.com>
2023-07-15 19:21:32 -0400

    start server and test in ci

--- modules/service/test/integration/helpers/Environment.ts
+++ modules/service/test/integration/helpers/Environment.ts
@@ -5,7 +5,3 @@ export const testingStage = (): string => process.env.STAGE || 'local';
 export const port = (): number => 3000;
 
 export const stackFile = (): string => `./stacks/${testingStage()}.stack.json`;
-
-export const isCITest = (): boolean => !!process.env.CI;
-
-export const shouldStartLocalServer = (): boolean => isCITest() && isLocalTest();
