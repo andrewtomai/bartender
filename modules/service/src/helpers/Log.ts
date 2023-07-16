8d2d65f2e720125519261d7ebfa0cf8a40a1ca70
Andrew Tomai <andrew.tomai@acuitybrands.com>
2023-07-16 10:21:08 -0400

    Simplify log a ....lot

--- modules/service/src/helpers/Log.ts
+++ modules/service/src/helpers/Log.ts
@@ -1,97 +1,21 @@
 import * as R from 'ramda';
 import chalk from 'chalk';
-import { v4 as uuid } from 'uuid';
-import { performance } from 'perf_hooks';
-import * as Environment from './Environment';
-//= ==============================================
-const LOG_LEVELS = {
-    info: 4,
-    debug: 3,
-    warn: 2,
-    error: 1,
-};
+import { isExecutingInLambda } from './Environment';
+
 const isObjectOrArray = R.either(R.is(Array), R.is(Object));
 const colorizeAny = (colorizer) => (o) => (isObjectOrArray(o) ? colorizer(JSON.stringify(o, null, 2)) : colorizer(o));
-const cyan = colorizeAny(chalk.cyan);
-const green = colorizeAny(chalk.green);
-const orange = colorizeAny(chalk.yellow);
-const red = colorizeAny(chalk.bold.red);
-class Logger {
-    logLevel: number;
-    colorize: boolean;
-    timeTable: Record<string, { start: number; message: string }>;
-    // internal variables:
-    // logLevel :: int
-    // colorize :: boolean
-    constructor(logLevel = process.env.LOG_LEVEL) {
-        this.logLevel = Environment.isExecutingInLambda() ? 1000 : Number(logLevel);
-        this.colorize = !Environment.isExecutingInLambda();
-        this.timeTable = {};
-    }
 
-    time(message) {
-        const label = uuid();
-        if (this.logLevel >= LOG_LEVELS.debug) {
-            this.timeTable[label] = { start: performance.now(), message };
-        }
-        return label;
-    }
+const log =
+    (color) =>
+    (...args) => {
+        const lines = isExecutingInLambda() ? args : R.map(colorizeAny(color), args);
+        console.log(...lines);
+    };
 
-    timeEnd(label) {
-        if (this.logLevel >= LOG_LEVELS.debug) {
-            try {
-                const { start, message } = this.timeTable[label];
-                const end = performance.now();
-                this.info(`${message}: ${end - start}ms`);
-                return end - start;
-            } catch (e) {
-                this.warn('Missing timer label: ', label);
-            }
-        }
-    }
+const Logger = {
+    info: log(chalk.green),
+    warn: log(chalk.yellow),
+    error: log(chalk.bold.red),
+};
 
-    colorizeArgs(colorizer, args) {
-        return this.colorize ? R.map(colorizer, args) : args;
-    }
-
-    separator(title) {
-        const seperatorLength = 80;
-        const tildesToUse = seperatorLength - title.length - 2;
-        const tildeBuffer = '~'.repeat(tildesToUse / 2);
-        this.warn();
-        this.warn(`${tildeBuffer} ${title} ${tildeBuffer}`);
-    }
-
-    info(...args) {
-        if (this.logLevel >= LOG_LEVELS.info) {
-            console.log(...this.colorizeArgs(cyan, args));
-        }
-    }
-
-    success(...args) {
-        if (this.logLevel >= LOG_LEVELS.debug) {
-            console.log(...this.colorizeArgs(green, args));
-        }
-    }
-
-    warn(...args) {
-        if (this.logLevel >= LOG_LEVELS.warn) {
-            console.warn(...this.colorizeArgs(orange, args));
-        }
-    }
-
-    error(...args) {
-        if (this.logLevel >= LOG_LEVELS.error) {
-            console.error(...this.colorizeArgs(red, args));
-        }
-    }
-}
-// let logger = null;
-// const getLogger = (logLevel) => {
-//     if (!logger) {
-//         logger = new Logger(logLevel);
-//     }
-//     return logger;
-// };
-// getLogger();
-export default new Logger();
+export default Logger;
